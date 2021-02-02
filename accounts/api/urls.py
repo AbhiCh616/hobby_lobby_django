edf7cd18aff6d861c3d41e742ec2efdffd609be2
Abhishek Chouhan <abhich1607@gmail.com>
2021-02-03 00:59:09 +0530

    Token log in added

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,6 +3,9 @@ from accounts.api.views import(
     UserCreate
 )
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('create/', UserCreate.as_view(), name='create'),
+    path('login/', obtain_auth_token, name='login'),
 ]
