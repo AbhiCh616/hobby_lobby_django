267895b29eceb208c04b42c8b589849218985e68
Abhishek Chouhan <abhich1607@gmail.com>
2021-02-03 00:39:30 +0530

    Token creation on registration

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None):
         if not email:
@@ -53,3 +58,8 @@ class User(AbstractBaseUser):
         # All admins are staff
         return self.is_admin
 
+# Create token automatically when user is created
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
